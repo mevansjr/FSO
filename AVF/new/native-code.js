c8bad034047114b8d219956ac3857e70e7bb5b55
Mark Evans <mevansjr@gmail.com>
2012-03-08 10:58:10 -0500

    jslint fix

--- AVF/new/native-code.js
+++ AVF/new/native-code.js
@@ -1,13 +1,11 @@
 // Mark Evans
 // AVF 1203
 // Native Code - iOS / Android - Phonegap
-
 //Geolocation Native Code --------------
 //
 //         onSuccess Callback
 //           This method accepts a `Position` object, which contains
 //           the current GPS coordinates
-        
         var onSuccess = function(position) {
             var getdiv = document.getElementById("spot");
             var newli  = document.createElement("li");
@@ -18,59 +16,45 @@
             var geolon = document.createTextNode('Longitude: '+ position.coords.longitude + '\n');
             console.log(position.coords.longitude);
             var geotim = document.createTextNode('Timestamp: '+ new Date(position.timestamp) + '\n');
-            
             newli.appendChild(geolat);
             getdiv.appendChild(newli);
-            
             newli2.appendChild(geolon);
             getdiv.appendChild(newli2);
-            
             newli3.appendChild(geotim);
             getdiv.appendChild(newli3);
-        };
-        
+        };      
          //onError Callback receives a PositionError object
-        
         function onError(error) {
             alert('code: '    + error.code    + '\n' +
                   'message: ' + error.message + '\n');
         }
-        
        navigator.geolocation.getCurrentPosition(onSuccess, onError, { maximumAge: 8000, timeout: 10000, enableHighAccuracy: true });
-//  
-      
+//       
 //Notification Native Code ------------
-        
         // Wait for PhoneGap to load
         //
         document.addEventListener("deviceready", onDeviceReady, false);
-        
         // PhoneGap is ready
         //
         function onDeviceReady() {
             // Empty
         }
-        
         // Show a custom alert
         //
         function showAlert() {
             navigator.notification.alert("Blah! This is an Alert.");
         }
-        
         // Beep three times
         //
         function playBeep() {
             navigator.notification.beep(3);
         }
-        
         // Vibrate for 2 seconds
         //
         function vibrate() {
             navigator.notification.vibrate(2000);
         }
-        
 //Camera Native Code ----------------
-
         function captureSuccess(mediaFiles) {
             navigator.notification.alert('Success taking picture:');
         }
@@ -81,14 +65,11 @@
                          $("#takePhotoButton").bind("tap", function() {
                                                     navigator.device.capture.captureImage(captureSuccess, captureError, {limit: 1});
                                                     });
-                         });
-                         
+                         });                         
 //Contact Native Code ----------------
-
         // Wait for PhoneGap to load
         //
-        document.addEventListener("deviceready", onDeviceReady, false);
-        
+        document.addEventListener("deviceready", onDeviceReady, false);  
         // PhoneGap is ready
         //
         function onDeviceReady() {
@@ -96,12 +77,10 @@
             var options = new ContactFindOptions();
             options.filter=""; // empty search string returns all contacts
             options.multiple=true; // return multiple results
-            filter = ["displayName"]; // return contact.displayName field
-            
+            filter = ["displayName"]; // return contact.displayName field           
             // find contacts
             navigator.contacts.find(filter, onSuccess, onError, options);
-        }
-        
+        }      
         // onSuccess: Get a snapshot of the current contacts
         //
         function onSuccess(contacts) {
