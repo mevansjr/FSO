b81d6e7a97f14a4c1897b920f5075c244fea27ed
Mark Evans <mevansjr@gmail.com>
2012-03-26 10:05:56 -0400

    Added files for Practice 1 :: testApp

---
+++ AOC 1/testApp/testAppTests/testAppTests.m
@@ -0,0 +1,32 @@
+//
+//  testAppTests.m
+//  testAppTests
+//
+//  Created by Mark Evans on 3/26/12.
+//  Copyright (c) 2012 MdTA / Full Sail University. All rights reserved.
+//
+
+#import "testAppTests.h"
+
+@implementation testAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in testAppTests");
+}
+
+@end
