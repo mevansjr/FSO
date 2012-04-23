022265ccaded3b3dfedf0ee0456bcaa282707767
Mark Evans <mevansjr@gmail.com>
2012-04-23 10:58:11 -0400

    Added text, input, button, data to ViewController.m

--- AOC 1/project4/project4/ViewController.m
+++ AOC 1/project4/project4/ViewController.m
@@ -13,6 +13,47 @@
 
 @implementation ViewController
 
+- (void)viewDidLoad
+{
+    UILabel *textLabel = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 10.0f, 100.0f, 30.0f)];
+    if (textLabel != nil)
+    {
+        textLabel.text = @"Username: ";
+        [self.view addSubview:textLabel];
+    }
+    textField = [[UITextField alloc] initWithFrame:CGRectMake(100.0f, 10.0f, 200.0f, 30.0f)];
+    if (textField != nil)
+    {
+        textField.borderStyle = UITextBorderStyleRoundedRect;
+        [self.view addSubview:textField];
+    }
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    if (button != nil)
+    {
+        button.frame = CGRectMake(225.0f, 50.0f, 75.0f, 30.0f);
+        [button setTitle:@"Login" forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(onClick) forControlEvents: UIControlEventTouchUpInside];
+        [self.view addSubview:button];
+    }
+    textStatusLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 100.0f, 320.0f, 80.0f)];
+    if (textStatusLabel != nil)
+    {
+        textStatusLabel.text = @"Please Enter Username ";
+        textStatusLabel.backgroundColor = [UIColor lightGrayColor];
+        textStatusLabel.textColor = [UIColor blueColor];
+        textStatusLabel.textAlignment = UITextAlignmentCenter;        
+        [self.view addSubview:textStatusLabel];
+    }
+    UIButton *showDateButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    if (showDateButton != nil)
+    {
+        showDateButton.frame = CGRectMake(10.0f, 250.0f, 125.0f, 50.0f);
+        [showDateButton setTitle:@"Show Date" forState:UIControlStateNormal];
+        [showDateButton addTarget:self action:@selector(showDate) forControlEvents: UIControlEventTouchUpInside];
+        [self.view addSubview:showDateButton];
+    }
+}
 
 - (void)viewDidUnload
 {
