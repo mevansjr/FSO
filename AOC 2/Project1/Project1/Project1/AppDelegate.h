21c45b4bb3a3fc74079d396ff234ed9aa8622586
Mark Evans <mevansjr@gmail.com>
2012-05-03 09:01:49 -0400

    Added Files :: Started Project 1

---
+++ AOC 2/Project1/Project1/Project1/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Project1
+//
+//  Created by Mark Evans on 5/3/12.
+//  Copyright (c) 2012 MdTA / Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AOC 2/Project1/Project1/Project1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Project1
+//
+//  Created by Mark Evans on 5/3/12.
+//  Copyright (c) 2012 MdTA / Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
