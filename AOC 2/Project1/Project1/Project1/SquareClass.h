28ce6ab7cb58b306051cd64ec229f2594653476d
Mark Evans <mevansjr@gmail.com>
2012-05-03 09:17:21 -0400

    Added SquareClass :: Proj1

---
+++ AOC 2/Project1/Project1/Project1/SquareClass.h
@@ -0,0 +1,15 @@
+//
+//  SquareClass.h
+//  Project1
+//
+//  Created by Mark Evans on 5/3/12.
+//  Copyright (c) 2012 MdTA / Full Sail University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ShapeClass.h"
+
+@interface SquareClass : ShapeClass
+
+
+@end
