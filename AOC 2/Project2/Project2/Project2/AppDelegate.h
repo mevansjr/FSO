1f9a207a75ae1b14a3c72f40cb3f07850d4a0804
Mark Evans <mevansjr@gmail.com>
2012-05-05 19:10:29 -0400

    added folder for project2

---
+++ AOC 2/Project2/Project2/Project2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Project2
+//
+//  Created by Mark Evans on 5/5/12.
+//  Copyright (c) 2012 MdTA / Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AOC 2/Project2/Project2/Project2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Project2
+//
+//  Created by Mark Evans on 5/5/12.
+//  Copyright (c) 2012 MdTA / Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
