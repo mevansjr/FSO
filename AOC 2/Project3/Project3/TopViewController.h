e3fa891f45a8b7d93aca5540b5bbf9101e73f77f
Mark Evans <mevansjr@gmail.com>
2012-05-14 11:35:59 -0400

    fix

--- AOC 2/Project3/Project3/TopViewController.h
+++ AOC 2/Project3/Project3/TopViewController.h
@@ -9,8 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface TopViewController : UIViewController
-{
-    
-}
 
 @end
--- AOC 2/Project3/Project3/TopViewController.m
+++ AOC 2/Project3/Project3/TopViewController.m
@@ -10,22 +10,42 @@
 
 @implementation TopViewController
 
-- (id)initWithFrame:(CGRect)frame
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super initWithFrame:frame];
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Initialization code
+        // Custom initialization
     }
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
+- (void)didReceiveMemoryWarning
 {
-    // Drawing code
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
 }
-*/
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
 
 @end
