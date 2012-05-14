cf887e438047f34e6c2a44322f3992f9c6a7c28b
Mark Evans <mevansjr@gmail.com>
2012-05-14 10:20:30 -0400

    AOC 2 :: Adding files for Project 3

---
+++ AOC 2/Project3/Project3/Project3/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Project3
+//
+//  Created by Mark Evans on 5/14/12.
+//  Copyright (c) 2012 MdTA / Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
