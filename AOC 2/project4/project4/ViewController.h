28e153944c37334696739986cf981315bb8cbf9a
Mark Evans <mevansjr@gmail.com>
2012-05-18 06:34:54 -0400

    Starting project 4

---
+++ AOC 2/project4/project4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  project4
+//
+//  Created by Mark Evans on 5/18/12.
+//  Copyright (c) 2012 MdTA / Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AOC 2/project4/project4/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  project4
+//
+//  Created by Mark Evans on 5/18/12.
+//  Copyright (c) 2012 MdTA / Full Sail University. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
