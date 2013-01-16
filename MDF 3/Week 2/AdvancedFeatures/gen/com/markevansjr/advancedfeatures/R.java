2959cca4ebcbffe2ab8310b9d8813a4d966c2172
Mark Evans <mevansjr@gmail.com>
2013-01-16 17:21:46 -0500

    update

--- MDF 3/Week 2/AdvancedFeatures/gen/com/markevansjr/advancedfeatures/R.java
+++ MDF 3/Week 2/AdvancedFeatures/gen/com/markevansjr/advancedfeatures/R.java
@@ -10,44 +10,79 @@ package com.markevansjr.advancedfeatures;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f050003;
+        public static final int tabDark=0x7f050001;
+        public static final int tabMedium=0x7f050000;
+        public static final int tabTransparent=0x7f050002;
+        public static final int white=0x7f050004;
+    }
     public static final class dimen {
-        public static final int padding=0x7f050000;
-        public static final int padding2=0x7f050001;
+        public static final int corner_radius=0x7f060002;
+        public static final int padding=0x7f060000;
+        public static final int padding2=0x7f060001;
+        public static final int tab_space=0x7f060004;
+        public static final int tab_space_bottom_line=0x7f060006;
+        public static final int tab_space_plus1=0x7f060005;
+        public static final int tab_space_top=0x7f060003;
+        public static final int tab_space_unselected_top=0x7f060007;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int app_background=0x7f020000;
+        public static final int home_normal=0x7f020001;
+        public static final int home_selected=0x7f020002;
+        public static final int ic_action_search=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int icon=0x7f020005;
+        public static final int search_normal=0x7f020006;
+        public static final int search_selected=0x7f020007;
+        public static final int tab_focus=0x7f020008;
+        public static final int tab_home=0x7f020009;
+        public static final int tab_indicator=0x7f02000a;
+        public static final int tab_press=0x7f02000b;
+        public static final int tab_search=0x7f02000c;
+        public static final int tab_selected=0x7f02000d;
+        public static final int tab_unselected=0x7f02000e;
+        public static final int tab_video=0x7f02000f;
+        public static final int video_normal=0x7f020010;
+        public static final int video_selected=0x7f020011;
     }
     public static final class id {
-        public static final int Button4=0x7f090003;
-        public static final int button1=0x7f090001;
-        public static final int button2=0x7f090000;
-        public static final int editText1=0x7f090004;
-        public static final int menu_settings=0x7f090005;
-        public static final int videoView1=0x7f090002;
+        public static final int Button4=0x7f0a0003;
+        public static final int button1=0x7f0a0001;
+        public static final int button2=0x7f0a0000;
+        public static final int editText1=0x7f0a0004;
+        public static final int icon=0x7f0a0005;
+        public static final int menu_settings=0x7f0a0007;
+        public static final int title=0x7f0a0006;
+        public static final int videoView1=0x7f0a0002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int home=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int tab_indicator=0x7f030003;
+        public static final int video=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_main=0x7f080000;
+        public static final int activity_main=0x7f090000;
     }
     public static final class raw {
         public static final int intro=0x7f040000;
     }
     public static final class string {
-        public static final int app_name=0x7f060000;
-        public static final int button=0x7f060005;
-        public static final int comment=0x7f060009;
-        public static final int enter=0x7f060008;
-        public static final int hello_world=0x7f060001;
-        public static final int locate=0x7f060007;
-        public static final int menu_settings=0x7f060002;
-        public static final int notification=0x7f060006;
-        public static final int playvideo=0x7f060004;
-        public static final int title_activity_main=0x7f060003;
+        public static final int app_name=0x7f070000;
+        public static final int button=0x7f070005;
+        public static final int comment=0x7f070009;
+        public static final int enter=0x7f070008;
+        public static final int hello_world=0x7f070001;
+        public static final int locate=0x7f070007;
+        public static final int menu_settings=0x7f070002;
+        public static final int notification=0x7f070006;
+        public static final int playvideo=0x7f070004;
+        public static final int title_activity_main=0x7f070003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f070000;
+        public static final int AppTheme=0x7f080000;
     }
 }
--- MDF 3/Week 2/AdvancedFeatures/src/com/markevansjr/advancedfeatures/Main.java
+++ MDF 3/Week 2/AdvancedFeatures/src/com/markevansjr/advancedfeatures/Main.java
@@ -1,138 +1,43 @@
 package com.markevansjr.advancedfeatures;
 
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.VideoView;
-import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.app.TabActivity;
 import android.content.Intent;
 
-public class Main extends Activity {
-	String _btxt;
-	MediaPlayer _mp;
-	Button _btn1;
-	VideoView _vv;
-	Context _context;
-	Notification _notification;
-	String _gpsMsg;
-	EditText _et;
-	double latitude;
-	double longitude;
+@SuppressWarnings("deprecation")
+public class Main extends TabActivity {
 
-	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.main);
+		setTabs() ;
+	}
+	
+	private void setTabs()
+	{
+		addTab("Home", R.drawable.tab_home, Home.class);
+		addTab("Media", R.drawable.tab_video, Video.class);
+	}
+	
+	private void addTab(String labelId, int drawableId, Class<?> c)
+	{
+		TabHost tabHost = getTabHost();
+		Intent intent = new Intent(this, c);
+		TabHost.TabSpec spec = tabHost.newTabSpec("tab" + labelId);	
 		
-		//// GROUP ONE <---------------------- ////
-		_context = this;
-		_mp = MediaPlayer.create(Main.this, R.raw.intro);
-		_btn1 = (Button) findViewById(R.id.button1);
-		_btxt = "Play";
+		View tabIndicator = LayoutInflater.from(this).inflate(R.layout.tab_indicator, getTabWidget(), false);
+		TextView title = (TextView) tabIndicator.findViewById(R.id.title);
+		title.setText(labelId);
+		ImageView icon = (ImageView) tabIndicator.findViewById(R.id.icon);
+		icon.setImageResource(drawableId);
 		
-		// AUDIO PLAYBACK
-		_btn1.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				if(_btxt.equals("Play")){
-					_mp.start();
-					_btxt = "Pause";
-					_btn1.setText(_btxt);
-				} else {
-					_mp.pause();
-					_btxt = "Play";
-					_btn1.setText(_btxt);
-				}
-			}
-		});
-		_btn1.setText(_btxt);
-		
-		// VIDEO PLAYBACK
-		final Button b2 = (Button) findViewById(R.id.button2);
-		b2.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				_vv = (VideoView) findViewById(R.id.videoView1);
-				String uripath = "android.resource://"+ getPackageName() + "/" + R.raw.intro;
-				_vv.setVideoURI(Uri.parse(uripath));
-				if(b2.getText().equals("Play Video")){
-					b2.setText("Pause Video");
-					_vv.start();
-				} else {
-					b2.setText("Play Video");
-					_vv.pause();
-				}
-			}
-		});
-		b2.setText("Play Video");
-		
-		//// GROUP TWO <---------------------- ////
-		// create class object
-    	GPSTracker gps = new GPSTracker(Main.this);
-    	// check if GPS enabled
-       	if(gps.canGetLocation()){
-       		latitude = gps.getLatitude();
-            longitude = gps.getLongitude();
-            // \n is for new line
-            //Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-            _gpsMsg ="GPS - Lat: "+latitude+" Lon: "+longitude;
-            
-       	}else{
-       		// can't get location
-            // GPS or Network is not enabled
-            // Ask user to enable GPS/network in settings
-            gps.showSettingsAlert();
-        }
-		
-		//// GROUP THREE <---------------------- ////
-		Button b4 = (Button) findViewById(R.id.Button4);
-		_et = (EditText) findViewById(R.id.editText1);
-		b4.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Log.i("TAG", "HELLO");
-				NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-				NotificationCompat.Builder nbuilder = new NotificationCompat.Builder(_context)
-				.setSmallIcon(R.drawable.ic_launcher)
-				.setContentTitle(_et.getText().toString()+"..")
-				.setContentText(_gpsMsg);
-				
-				// Dynamic location via GPS
-				String url = "http://maps.google.com/?q="+latitude+", "+longitude;
-				Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-				
-				webIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-				
-				PendingIntent pendingIntent = PendingIntent.getActivity(_context, 0, webIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-				
-				nbuilder.setContentIntent(pendingIntent);
-				_notification = nbuilder.build();
-				
-				_notification.tickerText = "New Comment!";
-				nm.notify(0, _notification);
-				
-				// Hide Keyboard
-				InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-				imm.hideSoftInputFromWindow(_et.getWindowToken(), 0);
-				
-				// Clear search field
-				_et.setText("");
-			}
-		});
-
+		spec.setIndicator(tabIndicator);
+		spec.setContent(intent);
+		tabHost.addTab(spec);
 	}
 }
